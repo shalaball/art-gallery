f7584b458ab3336c8421cfc3a9726c0706ce749b
Lew Tucker <lew.tucker@gmail.com>
2026-02-18 10:35:43 -0800

    Add labels file, update title to Napa Valley Horse Show 2008

---
+++ labels.js
@@ -0,0 +1,23 @@
+// Edit the text between the quotes to change each photo's label.
+// Order matches the photos left-to-right, top-to-bottom in the gallery.
+
+const LABELS = [
+  "Before the show",   // Slide 1  DSC_0005
+  "Solitaire",         // Slide 2  DSC_0006
+  "Untitled 3",        // Slide 3  DSC_0007
+  "Untitled 4",        // Slide 4  DSC_0008
+  "Untitled 5",        // Slide 5  DSC_0009
+  "Untitled 6",        // Slide 6  DSC_0010
+  "Untitled 7",        // Slide 7  DSC_0011
+  "Untitled 8",        // Slide 8  DSC_0012
+  "Untitled 9",        // Slide 9  DSC_0013
+  "Untitled 10",       // Slide 10 DSC_0014
+  "Untitled 11",       // Slide 11 DSC_0015
+  "Untitled 12",       // Slide 12 DSC_0016
+  "Untitled 13",       // Slide 13 DSC_0017
+  "Untitled 14",       // Slide 14 DSC_0018
+  "Untitled 15",       // Slide 15 DSC_0019
+  "Untitled 16",       // Slide 16 DSC_0020
+  "Untitled 17",       // Slide 17 DSC_0021
+  "Untitled 18",       // Slide 18 DSC_0022
+];
