e36f4c98e5fa9aa4a7ed92e6719962dc56c73c8b
Lew Tucker <lew.tucker@gmail.com>
2026-03-04 10:50:39 -0800

    Update gallery content

--- page-1/labels.js
+++ page-1/labels.js
@@ -2,13 +2,13 @@
 // To update labels or order, use the admin UI or edit CONTENT.md.
 
 const LABELS = [
+  { filename: "22C10F17-3C11-49C7-8E31-3913FC97D56E_1_105_c.jpeg", title: "Untitled", desc: "" },
   { filename: "IMG_2997.jpeg", title: "Storm on the Sea", desc: "" },
   { filename: "IMG_3469.jpeg", title: "Untitled 5", desc: "" },
   { filename: "IMG_3233.jpeg", title: "Untitled 3", desc: "" },
   { filename: "IMG_3189.jpeg", title: "Untitled 2", desc: "" },
   { filename: "IMG_3424.jpeg", title: "Untitled 4", desc: "" },
   { filename: "IMG_9358.jpeg", title: "Untitled 6", desc: "" },
-  { filename: "22C10F17-3C11-49C7-8E31-3913FC97D56E_1_105_c.jpeg", title: "Untitled", desc: "" },
   { filename: "image.jpeg", title: "Untitled", desc: "" },
   { filename: "IMG_2368.jpeg", title: "", desc: "" },
   { filename: "IMG_6221.jpeg", title: "", desc: "" },
