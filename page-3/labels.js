769b0bc568eb532e6d82dfa156cc119c9a1667e8
Shala Ball <shalaball@MacBook-Air.local>
2026-03-12 18:15:05 -0700

    Update gallery content

--- page-3/labels.js
+++ page-3/labels.js
@@ -13,7 +13,8 @@ const LABELS = [
   { filename: "IMG_6219.jpeg", title: "Impromptu #4", desc: "Acrylic on Canvas, 14\"x11\"" },
   { filename: "IMG_0570.jpeg", title: "The Secret Red", desc: "Alcohol Ink on Yupo Paper, 4\"x6\"" },
   { filename: "AD21225A-EB38-4FE3-884B-A01928051BA6_1_105_c.jpeg", title: "The Secret Red", desc: "Alcohol Ink on Yupo Paper, 4\"x6\"" },
-  { filename: "IMG_5991.jpeg", title: "Greenland's House", desc: "Acrylic Ink on Paper, 8\"x10\"" },
   { filename: "22C10F17-3C11-49C7-8E31-3913FC97D56E_1_105_c.jpeg", title: "Saturday on the F", desc: "Photo Collage, 14\"x11\"" },
+  { filename: "EEBF050B-5D99-458F-B192-5CC4BC9D5F02.png", title: "Saturday on The F", desc: "Photo Collage, 14\" x 11\"" },
+  { filename: "IMG_5991.jpeg", title: "At Greenland's House", desc: "Acrylic Ink on Paper, 8\"x10\"" },
   { filename: "IMG_4869.jpeg", title: "Untitled", desc: "Ink on Paper Collage, 8\" x 10\"" },
 ];
