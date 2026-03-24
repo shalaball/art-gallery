d49f180e3f68da320c99c4c539d9a5a5c76647e5
Shala Ball <shalaball@MacBook-Air.local>
2026-03-23 21:52:31 -0700

    Update gallery content

--- page-3/labels.js
+++ page-3/labels.js
@@ -7,7 +7,7 @@ const LABELS = [
   { filename: "C015E4F8-F2D5-47A7-995F-B7A9C3FEA8D4.png", title: "", desc: [] },
   { filename: "IMG_6350.jpeg", title: "All of Our Days", desc: ["Alcohol Ink on Yupo Paper","30 × 19 inches","2025","Signed"] },
   { filename: "8452FC30-6F6C-48E1-B9C4-9E6051876814.jpeg", title: "", desc: [] },
-  { filename: "AD21225A-EB38-4FE3-884B-A01928051BA6_1_105_c.jpeg", title: "", desc: [] },
   { filename: "IMG_0570.jpeg", title: "The Secret Red", desc: ["Alcohol Ink on Yupo Paper","4 × 6 inches","2024"] },
+  { filename: "AD21225A-EB38-4FE3-884B-A01928051BA6_1_105_c.jpeg", title: "", desc: [] },
   { filename: "IMG_5991.jpeg", title: "Greenland's House", desc: ["Acrylic Ink on Paper","8 × 10 inches","2025","Signed"] },
 ];
